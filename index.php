dcb011797b2a1a29ebb2ebf812cde22bb41cad06
chanie55 <anya.sc30@gmail.com>
2023-03-30 23:00:37 -0700

    home page update

--- index.php
+++ index.php
@@ -17,5 +17,32 @@
                 <a href = "" class = "register-button"> Register </a>
             </nav>
         </header>
+
+        <div class = container> 
+                <div class = "heading"> 
+                    <p class = "label"> Rent and Post Properties in the fastest way! </p>
+
+                    <p class = "content"> "This web portal will help you in searching available
+                    residential and commercial properties in General Santos City. Property owners
+                    will be able to post their property to publicize their business to the people.
+                    This is a place where owner and seeker will easily find each other." </p>
+                </div>
+
+            <div class = "image"> </div>
+        </div>
+
+        <div class = "box-container"> 
+            <div class = "first-box"> 
+                <p> I am the first content </p>
+            </div>
+
+            <div class = "second-box"> 
+                <p> I am the second content </p>
+            </div>
+
+            <div class = "third-box"> 
+                <p> I am the third content </p>
+            </div>
+        </div>
     </body>
 </html>
