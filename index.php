461e2652f3feb85ec622fd7a5eb983287673e8fb
chanie55 <anya.sc30@gmail.com>
2023-03-30 20:48:58 -0700

    header

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE>
+<html> 
+    <head> 
+        <title> Home </title>
+        <style>
+            <?php
+                include "css/style.css"
+            ?>
+        </style>
+    </head>
+
+    <body> 
+        <header>
+            <h3> RENTIN </h3>
+            <nav class = "login-navigation"> 
+                <a href = "" class = "login-button"> Login </a>
+                <a href = "" class = "register-button"> Register </a>
+            </nav>
+        </header>
+    </body>
+</html>
