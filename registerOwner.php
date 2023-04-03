4a2c90972acf9ce507dd930adc5e1ec3731e1a24
AjLynn <jusayan.139095@gensan.sti.edu.ph>
2023-04-03 21:36:36 +0800

    register owner partial

---
+++ registerOwner.php
@@ -0,0 +1,90 @@
+<!DOCTYPE>
+<html> 
+    <head> 
+        <meta name = "viewport" content = "width = device-width, initial-scale=1.0">
+        <title> Register </title>
+        <style>
+            <?php
+                include "css/registerOwner.css"
+            ?>
+        </style>
+    </head>
+
+    <body> 
+        <header>
+            <h3> RENTIN </h3>
+            <nav class = "login-navigation"> 
+                <a href = "" class = "login-button"> Login </a>
+                <a href = "" class = "register-button"> Register </a>
+            </nav>
+        </header>
+
+            <div class="container">
+                <div class="content">
+                    <div class="title"><h1>Register</h1></div>
+                        <div class="input_field">
+                            <label>First Name</label>
+                            <input type="text" class="input" placeholder="Enter your first name" required>
+                        </div>
+
+                        <div class="input_field">
+                            <label>Last Name</label>
+                            <input type="text" class="input" placeholder="Enter your last name" required>
+                        </div>
+
+                        <div class="input_field">
+                            <label>Email</label>
+                            <input type="text" class="input" placeholder="Enter your email" required>
+                        </div>
+
+                        <div class="input_field">
+                            <label>Contact</label>
+                            <input type="text" class="input" placeholder="Enter your contact" required>
+                        </div>
+
+                        <div class="input_field">
+                            <label>Username</label>
+                            <input type="username" class="input" placeholder="Enter your username" required>
+                        </div>
+
+                        <div class="input_field">
+                            <label>Password</label>
+                            <input type="password" class="input" placeholder="Enter your password" required>
+                        </div>
+
+                        <div class="input_field">
+                            <label>Confirm Password</label>
+                            <input type="password" class="input" placeholder="Confirm your password" required>
+                        </div> 
+
+                        <div class="checkbox-dropdown">
+                            Property Documents
+                            <ul class="checkbox-dropdown-list">
+                                <li>
+                                    <label>
+                                        <input type="checkbox" value="permit" name="permit"/> Business Permit </label>
+                                </li>
+                                <li>
+                                    <label>
+                                        <input type="checkbox" value="DTI" name="DTI"/> DTI Permit </label>
+                                </li>
+                                <li>
+                                    <label>
+                                        <input type="checkbox" value="BIR" name="BIR"/> 0605 BIR Form  </label>
+                                </li>
+                            </ul>
+                        </div>   
+
+                        <div class="button">
+                            <input type="submit" value="Register" class="btn">
+                        </div>
+                    </div>
+                    <div>
+                        <div class = "image"> </div>
+                        </div>
+                </div>
+      </form>
+    </div>
+  </div>
+    </body>
+</html>
