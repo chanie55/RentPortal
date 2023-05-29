a24b2f3ea6c068bf49e1b7f73a9ce637e78ad460
chanie55 <anya.sc30@gmail.com>
2023-05-28 23:45:06 -0700

    upp

---
+++ userdata.php
@@ -0,0 +1,34 @@
+<?php 
+    include "dbconn.php";
+
+    $userid = $_POST['userid'];
+
+    $sql = "SELECT *, user.user_ID, CONCAT(firstName,' ', lastName) AS fullName FROM reservation JOIN user ON reservation.user_ID = user.user_ID 
+                JOIN userinfo ON userinfo.userinfo_ID = user.userInfo_ID WHERE user.user_ID = '$userid'";
+    $result = mysqli_query($conn, $sql);
+
+    while ($row = mysqli_fetch_assoc($result)) {
+    ?>
+        <table width = '100%' bordered = '0'>
+            <tr> 
+                <td width = "100"><p> Proof of Payment</p><img src = "<?php echo "images/proof/".$row['proof']; ?>" style = "width: 250px; height: 330px;">
+                <td style = "padding: 10px;"> 
+                    <p> Name : <?php echo $row['fullName']; ?></p>
+                    <p> Email : <?php echo $row['email']; ?></p>
+                    <p> Contact : <?php echo $row['contact']; ?></p>
+                    <p> Amount : <?php echo $row['amount']; ?></p>
+                    <p> Mode of Payment : <?php echo $row['mop']; ?></p>
+                    <p> Status : <?php echo $row['status']; ?></p>
+                </td>
+            </tr> 
+
+        </table>
+        <form method = "POST" action = "confirmres.php"> 
+            <input type = "hidden" name = "useremail" value = "<?php echo $row['email']; ?>"/>
+            <input type = "hidden" name = "id" value = "<?php echo $row['user_ID']; ?>"/> 
+            <a href="confirmres.php"><button type="submit" name = "okay" class="btn btn-primary">Confirm</button></a>
+            <a href=""><script> alert("test")</scipt><button type="submit" class="btn btn-secondary" name = "no" data-dismiss="modal">Cancel</button></a>
+        </form>
+    <?php } ?>
+
+    
